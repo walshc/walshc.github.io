bea7640b94a334395a0af1a2e95e1e173b5b1cff
Christoph Walsh <cbtwalsh@uvt.nl>
2024-10-04 09:15:30 +0200

    add block 5 template

---
+++ ebi-prog-exams/sample-exam-block-5-template.R
@@ -0,0 +1,18 @@
+
+snr <- 1234567  # replace with your own student number
+
+# Read in datasets:
+df1 <- read.csv("gdp-per-capita-growths.csv")
+df2 <- read.csv("lending-rates.csv")
+
+# Question 18
+
+
+a18 <- 0  # REPLACE 0 THE RIGHT COMMAND
+
+# Question 19
+
+
+a19 <- 0  # REPLACE 0 THE RIGHT COMMAND
+
+# Question 20
